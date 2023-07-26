deb27f6a659386115abb59578e3e4e6e9040e763
nohssam73 <nohssam73@gmail.com>
2023-07-26 17:58:26 +0900

    202307026

---
+++ src/main/java/com/ict/db/DAO.java
@@ -0,0 +1,24 @@
+package com.ict.db;
+
+import java.util.List;
+
+import org.mybatis.spring.SqlSessionTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Repository;
+
+
+public class DAO {
+	// 실제 사용할 클래스 ; SqlSessionTemplate
+	// jsp에서 사용한 SqlSession과 사용법 같다.
+	private SqlSessionTemplate session;
+
+	public SqlSessionTemplate getSession() {
+		return session;
+	}
+
+	public void setSession(SqlSessionTemplate session) {
+		this.session = session;
+	}
+	
+}
